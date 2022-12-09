5c2a8bb259be229e29a63f28f09a269f30288524
labuchaust <101212895+labuchaust@users.noreply.github.com>
2022-12-09 13:48:14 -0500

    hiaiii haiii hiya
    ithinktthiswokrs but i really odn tknow

---
+++ GAME28567-A3/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Your solution must make use of the following fields. If these values are changed, even at runtime,
+    /// the character controller should respect the new values and act as detailed in the Unity inspector.
+    /// </summary>
+
+   
+
+    
+
+    [SerializeField]
+    private float m_jumpApexHeight;
+
+    [SerializeField]
+    private float m_jumpApexTime;
+
+    [SerializeField]
+    private float m_terminalVelocity;
+
+    [SerializeField]
+    private float m_coyoteTime;
+
+    [SerializeField]
+    private float m_jumpBufferTime;
+
+    [SerializeField]
+    private float m_accelerationTimeFromRest;
+
+    [SerializeField]
+    private float m_decelerationTimeToRest;
+
+    [SerializeField]
+    private float m_maxHorizontalSpeed;
+
+    [SerializeField]
+    private float m_accelerationTimeFromQuickturn;
+
+    [SerializeField]
+    private float m_decelerationTimeFromQuickturn;
+
+    public enum FacingDirection { Left, Right }
+
+    public bool IsWalking()
+    {
+
+
+        throw new System.NotImplementedException( "IsWalking in PlayerController has not been implemented." );
+    }
+
+    public bool IsGrounded()
+    {
+      
+       throw new System.NotImplementedException( "IsGrounded in PlayerController has not been implemented." );
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        throw new System.NotImplementedException( "GetFacingDirection in PlayerController has not been implemented." );
+    }
+
+    // Add additional methods such as Start, Update, FixedUpdate, or whatever else you think is necessary, below.
+}
